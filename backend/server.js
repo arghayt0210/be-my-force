57510a4b4189d7c78e917425f32527749e94bdf2
arghayt0210 <argha.yt.0210@gmail.com>
2024-12-15 01:55:16 +0530

    backend prod change

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "https://be-myforce.onrender.com",
     methods: ["GET", "POST"],
   },
 });
